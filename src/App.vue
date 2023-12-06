da417ca24e92136886b0d0de8f37bb8a8dafc024
Idedav <xdavide.correa@gmail.com>
2023-12-06 14:55:47 +0100

    [init] readme

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name : 'App',
+  data(){
+    return{
+      title: 'Hello World'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
