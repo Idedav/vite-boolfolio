0569b3039c851da29f36359a59c5d42f3358e284
Idedav <xdavide.correa@gmail.com>
2023-12-06 16:45:28 +0100

    [add] navigator

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,39 @@
 import axios from 'axios';
 import { store } from './data/store';
 import ProjectCard from './components/ProjectCard.vue';
+import Loader from './components/Loader.vue';
+import Navigator from './components/Navigator.vue';
 
 export default {
   name : 'App',
   components:{
-    ProjectCard
+    ProjectCard,
+    Loader,
+    Navigator
   },
   data(){
     return{
       title: 'My Projects',
+      isLoaded: false,
+      links: []
     }
   },
 
   methods:{
-    getApi(){
-      axios.get(store.apiUrl + 'projects')
+    getApi(endPoint){
+      this.isLoaded = false;
+      axios.get(endPoint)
       .then(result =>{
-        console.log(result.data.data);
-        store.projects = result.data;
+        this.isLoaded = true;
+        console.log(result.data.links);
+        store.projects = result.data.data;
+        this.links = result.data.links;
       })
     }
   },
 
   mounted(){
-    this.getApi();
+    this.getApi(store.apiUrl + 'projects');
   }
 
 }
@@ -34,8 +43,12 @@ export default {
 
 <template>
   <h1 class="text-center my-5">{{ title }}</h1>
-  <div class="container d-flex flex-wrap justify-content-center">
-    <ProjectCard />
+  <div class="container d-flex justify-content-center align-items-center">
+    <Loader v-if="!isLoaded" />
+    <div class="d-flex flex-wrap justify-content-center" v-else>
+      <ProjectCard />
+      <Navigator :links="links" @callApi="getApi"/>
+    </div>
   </div>
 </template>
 
