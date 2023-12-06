44d13e6455b66805519b18b19e31745290760bbe
Idedav <xdavide.correa@gmail.com>
2023-12-06 15:17:23 +0100

    [add] store [add] function getApi()

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script>
+
+import axios from 'axios';
+import { store } from './data/store';
+
 export default {
   name : 'App',
   data(){
     return{
       title: 'Hello World'
     }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl + 'projects')
+      .then(result =>{
+        console.log(result.data);
+      })
+    }
+  },
+
+  mounted(){
+    this.getApi();
   }
+
 }
 </script>
 
